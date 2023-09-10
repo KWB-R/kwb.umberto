6b4ba96c8218c2006df6a11f211b6001c74e112e
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2023-09-10 13:43:13 +0200

    Move to_tables() and related to its own file

---
+++ R/to_tables.R
@@ -0,0 +1,207 @@
+# to_tables --------------------------------------------------------------------
+to_tables <- function(content)
+{
+  #content <- contents[[1L]]
+  
+  fetch <- kwb.utils::createAccessor(content)
+  
+  result <- list(
+    products = fetch("products") %>%
+      convert_and_bind(to_product) %>%
+      prefix_columns("product_"),
+    entries = fetch("entries") %>%
+      convert_and_bind(to_entry) %>%
+      kwb.utils::removeColumns("exchangeFullName") %>%
+      prefix_columns("entry_"),
+    processes = fetch("processes") %>%
+      convert_and_bind(to_process) %>%
+      prefix_columns("process_"),
+    places = fetch("places") %>%
+      convert_and_bind(to_place) %>%
+      prefix_columns("place_"),
+    indicators = fetch("indicators") %>%
+      convert_and_bind(to_indicator) %>%
+      prefix_columns("indicator_"),
+    scenarios = fetch("scenarios") %>%
+      convert_and_bind(to_scenario) %>%
+      prefix_columns("scenario_"),
+    evaluationMethods = fetch("evaluationMethods") %>%
+      convert_and_bind(to_evaluationMethod) %>%
+      prefix_columns("evaluationMethod_")
+  )
+  
+  structure(
+    result, 
+    timestamp = fetch("timestamp")
+  )
+}
+
+# to_product -------------------------------------------------------------------
+to_product <- function(x)
+{
+  #x <- contents[[1]]$products[[1]]
+  
+  flat <- get_flat_part(x)
+  
+  lcia_list <- get_remaining(x, flat) %>%
+    remove_zero_length_entries() %>%
+    kwb.utils::selectElements("lcia")
+  
+  #str(lcia_list)
+  
+  #convert_and_bind(lcia_list, get_flat_part)
+  
+  lcia <- lcia_list %>%
+    convert_and_bind(to_lcia) %>%
+    kwb.utils::renameColumns(list(
+      "entryId" = "lciaEntryId"
+    ))
+  
+  cbind(
+    flat %>%
+      remove_uuid(), 
+    lcia
+  )
+}
+
+# get_flat_part ----------------------------------------------------------------
+get_flat_part <- function(x)
+{
+  check_for_list(x)
+  
+  is_flat <- lengths(x) == 1L & !sapply(x, is.list)
+  
+  if (!any(is_flat)) {
+    return(NULL)
+  }
+  
+  as.data.frame(x[is_flat])
+}
+
+# get_remaining ----------------------------------------------------------------
+get_remaining <- function(x, flat_part)
+{
+  check_for_list(x)
+  check_for_data_frame(flat_part)
+  
+  x[setdiff(names(x), names(flat_part))]
+}
+
+# to_lcia ----------------------------------------------------------------------
+to_lcia <- function(x)
+{
+  #x <- lcia_list[[2L]]
+  flat <- get_flat_part(x)
+  
+  remaining <- get_remaining(x, flat) %>%
+    remove_zero_length_entries()
+  
+  #str(remaining, 2)
+  
+  get_inout <- function(side) {
+    sublist <- remaining[[paste0(side, "s")]]
+    if (!is.null(sublist)) {
+      sublist %>%
+        convert_and_bind(get_flat_part) %>%
+        cbind(side = side)
+    } # else NULL
+  }
+  
+  inputs <- get_inout("input")
+  outputs <- get_inout("output")
+  
+  cbind(flat, rbind(inputs, outputs))
+}
+
+# to_entry ---------------------------------------------------------------------
+to_entry <- function(xz)
+{
+  #xz <- contents[[1]]$entries[[1]]
+  flat <- get_flat_part(xz)
+  
+  kwb.utils::removeColumns(flat, c(
+    "X.id",
+    "exchangeId", 
+    "classificationId"
+  )) 
+}
+
+# to_process -------------------------------------------------------------------
+to_process <- function(x)
+{
+  x %>%
+    get_flat_part() %>%
+    remove_xid()
+}
+
+# remove_xid -------------------------------------------------------------------
+remove_xid <- function(df)
+{
+  kwb.utils::removeColumns(df, "X.id")
+}
+
+# remove_uuid ------------------------------------------------------------------
+remove_uuid <- function(df)
+{
+  kwb.utils::removeColumns(df, "uuid")
+}
+
+# to_place ---------------------------------------------------------------------
+to_place <- function(x)
+{
+  x %>%
+    get_flat_part() %>%
+    remove_xid()
+}
+
+# to_scenario ------------------------------------------------------------------
+to_scenario <- function(x)
+{
+  x %>%
+    get_flat_part() %>%
+    remove_xid()
+}
+
+# to_evaluationMethod ----------------------------------------------------------
+to_evaluationMethod <- function(x)
+{
+  x %>%
+    get_flat_part() %>%
+    remove_xid() %>%
+    remove_uuid()
+}
+
+# to_indicator -----------------------------------------------------------------
+to_indicator <- function(x)
+{
+  #x <- contents[[1L]]$indicators[[1L]]
+  flat <- get_flat_part(x)
+  
+  path_parts <- get_remaining(x, flat) %>%
+    kwb.utils::selectElements("path")
+  
+  stopifnot(all(lengths(path_parts) == 1L))
+  
+  flat %>%
+    remove_uuid() %>%
+    cbind(indicatorPath = paste0(path_parts[[1L]], "->", path_parts[[2L]]))
+}
+
+# convert_and_bind -------------------------------------------------------------
+convert_and_bind <- function(x_list, converter)
+{
+  lapply(x_list, converter) %>%
+    kwb.utils::safeRowBindAll()
+}
+
+# flatten_list_of_unnamed_elements ---------------------------------------------
+flatten_list_of_unnamed_elements <- function(x)
+{
+  stopifnot(!is.null(names(x)))
+  
+  # We expect all elements to be lists with identical names
+  stopifnot(all_have_identical_names(x))
+  
+  do.call(rbind, lapply(x, flatten))
+}
+
