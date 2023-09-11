d00f819549d03a8e4ac717eb9418496f17e83545
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2023-09-11 08:24:24 +0200

    Use flatten() on a higher level, check identity

--- R/to_tables.R
+++ R/to_tables.R
@@ -19,26 +19,71 @@ to_tables <- function(content)
     convert_and_bind(to_entry) %>%
     kwb.utils::removeColumns("exchangeFullName") %>%
     prefix_columns("entry_")
+
+  entries2 <- fetch("entries") %>%
+    flatten() %>%
+    kwb.utils::removeColumns(pattern = "(\\.i|I)d$") %>%
+    kwb.utils::removeColumns("exchangeFullName") %>%
+    prefix_columns("entry_")
   
+  check_identity(entries, entries2)
+
   processes <- fetch("processes") %>%
     convert_and_bind(to_process) %>%
     prefix_columns("process_")
+
+  processes2 <- fetch("processes") %>%
+    flatten() %>%
+    remove_xid() %>%
+    prefix_columns("process_")
   
+  check_identity(processes, processes2)
+
   places <- fetch("places") %>%
     convert_and_bind(to_place) %>%
     prefix_columns("place_")
   
+  places2 <- fetch("places") %>%
+    flatten() %>%
+    remove_xid() %>%
+    prefix_columns("place_")
+  
+  check_identity(places, places2)
+  
   indicators <- fetch("indicators") %>%
     convert_and_bind(to_indicator) %>%
     prefix_columns("indicator_")
+
+  indicators2 <- fetch("indicators") %>%
+    flatten(x, sep = "->") %>%
+    remove_uuid() %>%
+    kwb.utils::renameColumns(list(path = "indicatorPath")) %>%
+    prefix_columns("indicator_")
+
+  check_identity(indicators, indicators2)
   
   scenarios <- fetch("scenarios") %>%
     convert_and_bind(to_scenario) %>%
     prefix_columns("scenario_")
+
+  scenarios2 <- fetch("scenarios") %>%
+    flatten() %>%
+    remove_xid() %>%
+    prefix_columns("scenario_")
+
+  check_identity(scenarios, scenarios2)
   
   evaluationMethods <- fetch("evaluationMethods") %>%
     convert_and_bind(to_evaluationMethod) %>%
     prefix_columns("evaluationMethod_")
+  
+  evaluationMethods2 <- fetch("evaluationMethods") %>%
+    flatten() %>%
+    remove_xid() %>%
+    remove_uuid() %>%
+    prefix_columns("evaluationMethod_")
+  
+  check_identity(evaluationMethods, evaluationMethods2)
   
   result <- list(
     products = products,
